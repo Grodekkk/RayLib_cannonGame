b462148b68b85416e5e61ebd8ad7e54ec1695a9d
Grodekkk <grodek7483@gmail.com>
2026-02-11 17:49:44 +0100

    reworking direction change logic

--- Ball.cpp
+++ Ball.cpp
@@ -6,49 +6,68 @@ using namespace std;
 class Ball
 {
 private:
-	int positiveShift = 3;
+	int positiveShift = 3;          //this way it will be always 45 degrees
     int negativeShift = -3;
-    int speed;
-	int xPos;		//initial position stated in constructor
-	int yPos;
-	int radius;
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-
-	
-
-    //this is straight up wrong, will make ball always go rightdown
+    //this is straight up wrong, will make ball always go rightdown <is it?>
     int xShift = 3;
     int yShift = 3;
 
+
+	int xPos;		//initial position stated in constructor
+	int yPos;
+	int radius;
+    int screenWidth;    
+    int screenHeight;
+
+	
+
+ 
+
 public:
-    enum Level {
+    //enumeration of directions ball follows
+    enum Level 
+    {
         RIGHTDOWN,
         RIGHTUP,
         LEFTUP,
         LEFTDOWN
     };
 
+    //current direction of the ball
     Level currentDir;  //also need to be stated in the constructor
 
-    Ball(int x, int y, float r, Level dir = RIGHTDOWN)
-        : xPos(x), yPos(y), radius(r), currentDir(dir)
+    //constructor of an object
+    Ball(int x, int y, float r, int screenW, int screenH, Level dir = RIGHTUP)
+        : xPos(x), yPos(y), radius(r), screenWidth(screenW), screenHeight(screenH), currentDir(dir)
     {
         // ustaw shift zgodnie z dir (albo zawołaj changeDirection "od zera")
         xShift = positiveShift;
         yShift = positiveShift;
     }
 
+    //classic change direction function, works like "cyclic circle (RD->RU->LU->LD->RD->...)"
     void changeDirection()
     {
         switch (currentDir)
         {
         case RIGHTDOWN:
-            currentDir = RIGHTUP;
-            xShift = positiveShift;
-            yShift = negativeShift;
-            cout << "zmiana pozycji z RIGHTDOWN na RIGHTUP" << endl;
-            break;
+            if (yPos >= screenHeight - ((radius * 3) + 5) )
+            {
+                currentDir = RIGHTUP;
+                xShift = positiveShift;
+                yShift = negativeShift;
+                cout << "[N] zmiana pozycji z RIGHTDOWN na RIGHTUP" << endl;
+                break;
+            }
+            else //kulka uderza w prawa scianke
+            {
+                currentDir = LEFTDOWN;
+                xShift = negativeShift;
+                yShift = positiveShift;
+                cout << " [N] zmiana pozycji LEFTDOWN" << endl;
+                break;
+            }
+            
         case RIGHTUP:
             currentDir = LEFTUP;
             xShift = negativeShift;
@@ -56,17 +75,69 @@ public:
             cout << "zmiana pozycji LEFTUP" << endl;
             break;
         case LEFTUP:
-            currentDir = LEFTDOWN;
-            xShift = negativeShift;
-            yShift = positiveShift;
-            cout << "zmiana pozycji LEFTDOWN" << endl;
-            break;
+            if (yPos < (radius * 4))
+            {
+                currentDir = LEFTDOWN;
+                xShift = negativeShift;
+                yShift = positiveShift;
+                cout << "zmiana pozycji LEFTDOWN" << endl;
+                break;
+            }
+            else
+            {
+                currentDir = RIGHTUP;
+                xShift = positiveShift;
+                yShift = positiveShift;
+                cout << "zmiana pozycji LEFTDOWN" << endl;
+                break;
+            }
+            
         case LEFTDOWN:
             currentDir = RIGHTDOWN;
             xShift = positiveShift;
             yShift = positiveShift;
             cout << "zmiana pozycji RIGHTDOWN" << endl;
             break;
+        }
+    }
+
+    //reworked function of changing direction, direction change no longer follows "circle pattern" instead, direction is changed to the direction passed as an argument
+    //possible problem: enum of direction is a part of a class. How method outside will know which change to pass?
+    //TODO: another method/function to correct out of bounds/ inside obstacle problem.
+    //metoda zwracająca odpowiedni direction po wykonaniu obliczen? poki co tylko na granice, skąd kulka zna granice mapy? (problem)
+    //czy ta cała logika ma sens?
+    void changeDirectionTo(Level targetDirection) //do we pass this correctly, relearn about pointers
+    {
+        switch (targetDirection)
+        {
+            //changes ball movement to right and down
+            case RIGHTDOWN:
+                    currentDir = RIGHTDOWN;
+                    xShift = positiveShift;
+                    yShift = positiveShift;
+                    cout << "direction changed to RIGHTDOWN" << endl;
+                    break;
+
+            case RIGHTUP:
+                currentDir = RIGHTUP;
+                xShift = positiveShift;
+                yShift = negativeShift;
+                cout << "direction changed to RIGHTUP" << endl;
+                break;
+
+            case LEFTUP:
+                currentDir = LEFTUP;
+                xShift = negativeShift;
+                yShift = negativeShift;
+                cout << "direction changed to LEFTUP" << endl;
+                break;
+
+            case LEFTDOWN:
+                currentDir = LEFTDOWN;
+                xShift = negativeShift;
+                yShift = positiveShift;
+                cout << "direction changed to LEFTDOWN" << endl;
+                break;
         }
     }
 
@@ -95,12 +166,18 @@ public:
         xPos = xPos + xShift;
         yPos = yPos + yShift;
     }
-//
+
     void checkForDirChange()
     {
         if (xPos <= 20 || xPos >= (screenWidth - 20) || yPos <= 20 || yPos >= (screenHeight - 20))
         {
             changeDirection();
         }
+    }
+
+    //ball itself checks for the out of bounds state, returns true or false <or direction to change> <correcting wrong coordinates> <struct>
+    bool isBallOutOfBounds()
+    {
+
     }
 };
