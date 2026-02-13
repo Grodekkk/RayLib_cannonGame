5a624a81a02a1f07ed21f7e65f780ce7265c28f9
Grodekkk <grodek7483@gmail.com>
2026-02-13 13:29:58 +0100

    Refactor ball handling and remove unused code

--- main.cpp
+++ main.cpp
@@ -5,104 +5,33 @@
 using namespace std;
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                       NOTES                                                                             //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+(1) theoretical borders for the ball:
+    x:  x<radius, x>width-radius
+    y:  y<radius, y>height-radius
+
+
+
+
+*/
+
+
+
 const int screenWidth = 800;
 const int screenHeight = 600;
 
-const int positiveShift = 3;
-const int negativeShift = -3;
-
-//BORDERY FRAME'A
-const int downBorder = screenHeight;
-const int rightBorder = screenWidth;
-const int upBorder = 0;
-const int leftBorder = 0;
-
-//initial position of the ball
-int xPos = 50;
-int yPos = 50;
-
-//PRZESUNIECIA
-int xShift = 3;
-int yShift = 3;
-
-//enumy kierunków
-enum Level {
-    RIGHTDOWN,
-    RIGHTUP,
-    LEFTUP,
-    LEFTDOWN
-};
-
-Level currentDir = RIGHTDOWN;
-
-
-void changeDirection(Level &currentDir)
-{
-    switch (currentDir)
-    {
-    case RIGHTDOWN:
-        currentDir = RIGHTUP;
-        xShift = positiveShift;
-        yShift = negativeShift;
-        /* //somehow this makes it ummmmmmmmm
-        if (yPos > (screenHeight - 20))
-        {
-            yPos = screenHeight - 20;
-        }
-        */
-        cout << "zmiana pozycji z RIGHTDOWN na RIGHTUP" << endl;
-        break;
-    case RIGHTUP:
-        currentDir = LEFTUP;
-        xShift = negativeShift;
-        yShift = negativeShift;
-        cout << "zmiana pozycji LEFTUP" << endl;
-        break;
-    case LEFTUP:
-        currentDir = LEFTDOWN;
-        xShift = negativeShift;
-        yShift = positiveShift;
-        cout << "zmiana pozycji LEFTDOWN" << endl;
-        break;
-    case LEFTDOWN:
-        currentDir = RIGHTDOWN;
-        xShift = positiveShift;
-        yShift = positiveShift;
-        cout << "zmiana pozycji RIGHTDOWN" << endl;
-        break;
-    }
-}
-
-void DrawCircleColor()
-{
-    switch (currentDir)
-    {
-    case RIGHTDOWN:
-        DrawCircle(xPos, yPos, 10, GREEN);
-        break;
-    case RIGHTUP:
-        DrawCircle(xPos, yPos, 10, RED);
-        break;
-    case LEFTUP:
-        DrawCircle(xPos, yPos, 10, SKYBLUE);
-        break;
-    case LEFTDOWN:
-        DrawCircle(xPos, yPos, 10, PINK);
-        break;
-    }
-    
-}
-
-void updateBallPosition()
-{
-    xPos = xPos + xShift;
-    yPos = yPos + yShift;
-}
-
 int main() {
-    std::vector<Ball> balls;
-    balls.emplace_back(120, 30, 15);
-    balls.emplace_back(25, 50, 5);
+    vector<Ball> balls;
+    balls.emplace_back(120, 30, 10, screenWidth, screenHeight);
+    balls.emplace_back(150, 30, 20, screenWidth, screenHeight);
+    balls.emplace_back(120, 200, 5, screenWidth, screenHeight);
+    balls.emplace_back(250, 90, 30, screenWidth, screenHeight);
+    balls.emplace_back(400, 30, 15, screenWidth, screenHeight);
+    balls.emplace_back(300, 30, 15, screenWidth, screenHeight);
+    //balls.emplace_back(25, 50, 5);
 
     InitWindow(screenWidth, screenHeight, "Kocham_Mysq.EXE");
     SetTargetFPS(60);
@@ -112,25 +41,19 @@ int main() {
     {           
         BeginDrawing();
         ClearBackground(BLACK);
+
+        //bounds testing
+        //DrawCircle(400, 30, 30, GREEN);
+        
         
         for (size_t i = 0; i < balls.size(); i++)
         {
-            balls[i].updateBallPosition();
-            balls[i].DrawCircleColor();
-            balls[i].checkForDirChange();
+            balls[i].updateBallPosition(); //shifts xy 
+            balls[i].DrawCircleColor();    //drawing procedure
+            balls[i].checkBorders();
         }
 
-            //drawing the circle
-            updateBallPosition();
             
-            DrawCircleColor();
-            if (xPos <= 20 || xPos >= (screenWidth - 20) || yPos <= 20 || yPos >= (screenHeight - 20))
-            {
-                changeDirection(currentDir);
-            }
-            //cout << "current dir: " << currentDir << endl;
-            
-        
         EndDrawing();
     }
     CloseWindow();
